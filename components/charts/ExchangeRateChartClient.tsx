7552ef0bc0abd251a12d3c8fbd7ec01622093478
Andres Faundez <a.a.faundez.ortega@gmail.com>
2025-12-12 20:58:20 -0300

    fix(chart): resolve typescript errors for yAxis and tooltip

--- components/charts/ExchangeRateChartClient.tsx
+++ components/charts/ExchangeRateChartClient.tsx
@@ -42,7 +42,7 @@ const ExchangeRateChartClient = ({ from, to, series }: Props) => {
     },
     yAxis: {
       title: {
-        text: null,
+        text: undefined,
       },
       gridLineColor: "rgba(255, 255, 255, 0.05)",
       labels: {
