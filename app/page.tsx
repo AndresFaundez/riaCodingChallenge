7913a22487010eeabc279cd4c643987d5c77d276
Andres Faundez <a.a.faundez.ortega@gmail.com>
2025-12-12 18:38:48 -0300

    Feat: add server-sude exchange rates overview with currencies fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import ExchangeOverview from "@/components/overview/ExchangeOverview"
 
 const Page = () => {
   return (
     <section>
       <h1 className = "text-center">The best currency exchange there is <br /> Event You Cant Miss</h1>
       <p className = "text-center mt-5">USD, EUR, and Many More, All in One Place</p>
-      
+      <ExchangeOverview />
     </section>
   )
 }
--- components/overview/ExchangeOverview.tsx
+++ components/overview/ExchangeOverview.tsx
@@ -1,10 +1,22 @@
 import ExchangeOverviewClient from "./ExchangeOverviewClient";
 import  {getExchangeRate, getCurrencies} from "@/lib/frankfurter";
 
-const ExchangeOverview = () => {
+const  defaultBase = "USD";
+const biggestCurrencies = [
+    "EUR", "GBP", "JPY", "AUD", "CAD",
+    "CHF", "CNY", "SEK", "NZD", "MXN",
+  ];
+  
+const ExchangeOverview = async  () => {
+  const rates = await getExchangeRate(defaultBase, biggestCurrencies);
+  const currencies = await getCurrencies();
   return (
-    <div>ExchangeOverview</div>
-  )
-}
+    <ExchangeOverviewClient 
+    currencies={currencies}
+    initialRates={rates}
+    initialBase={defaultBase}
+    symbols = {biggestCurrencies}/>
+  );
+};
 
-export default ExchangeOverview
+export default ExchangeOverview;
